878b750de32857107f45a3670eae0bbaa9313d67
Samson <Samsonho2005@gmail.com>
2026-03-10 23:54:03 +0800

    [refactor](convertor) Change convertor name to prevent conflict with annotation

--- app/src/main/java/com/monkey/focus_app/data/db/Converters.kt
+++ app/src/main/java/com/monkey/focus_app/data/db/Converters.kt
@@ -4,7 +4,7 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class Converter {
+class Converters {
     private val gson = Gson()
 
     @TypeConverter
