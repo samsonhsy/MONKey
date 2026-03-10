6ddebb23d0b919e403557f842f53a46e0e3db230
Samson <Samsonho2005@gmail.com>
2026-03-10 11:56:10 +0800

    [feat](db) Add Session and Tag dao in appdatabase

--- app/src/main/java/com/monkey/focus_app/data/db/AppDatabase.kt
+++ app/src/main/java/com/monkey/focus_app/data/db/AppDatabase.kt
@@ -3,6 +3,8 @@ package com.monkey.focus_app.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.monkey.focus_app.data.db.dao.TagDao
+import com.monkey.focus_app.data.db.dao.SessionDao
 import com.monkey.focus_app.data.db.dao.FocusLogDao
 import com.monkey.focus_app.data.db.dao.RewardItemDao
 import com.monkey.focus_app.data.db.dao.UserStatsDao
@@ -25,6 +27,8 @@ import com.monkey.focus_app.data.db.entitiy.UserStats
     TypeConverter::class
 )
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun tagDao(): TagDao
+    abstract fun sessionDao(): SessionDao
     abstract fun focusLogDao(): FocusLogDao
     abstract fun rewardItemDao(): RewardItemDao
     abstract fun userStatsDao(): UserStatsDao
