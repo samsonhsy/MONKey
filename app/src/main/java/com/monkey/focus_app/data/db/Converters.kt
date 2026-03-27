cc72400fab11a1b58ad05149e90a0eee295b5bbd
Samson <Samsonho2005@gmail.com>
2026-03-27 23:10:10 +0800

    [fix](data) Restore data dir in branch2 to main

---
+++ app/src/main/java/com/monkey/focus_app/data/db/Converters.kt
@@ -0,0 +1,22 @@
+package com.monkey.focus_app.data.db
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromJsonToListOfString(value: String?): List<String> {
+        if (value == null) return emptyList()
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun fromListOfStringToJson(list: List<String?>): String {
+        return gson.toJson(list)
+    }
+
+}
---
+++ app/src/main/java/com/monkey/focus_app/data/db/entity/FocusLog.kt
@@ -0,0 +1,26 @@
+package com.monkey.focus_app.data.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "focus_log",
+    foreignKeys = [
+        ForeignKey(
+            entity = Session::class,
+            parentColumns = ["session_id"],
+            childColumns = ["session_id"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ]
+)
+data class FocusLog(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "focus_log_id") val focusLogID: Int,
+    @ColumnInfo(name = "session_id") val sessionID: Int?,
+    @ColumnInfo(name = "actual_start_time") val actualStartTime: Long,
+    @ColumnInfo(name = "actual_end_time") val actualEndTime: Long,
+    @ColumnInfo(name = "was_hard_unlocked") val wasHardUnlocked: Boolean,
+    @ColumnInfo(name = "points_earned") val pointsEarned: Int
+)
