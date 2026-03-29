00aed45573f4f5c900cc163efd8e20e929671ced
Samson <Samsonho2005@gmail.com>
2026-03-29 22:35:24 +0800

    [feat](viewmodel) Add Home and Session view model factory

---
+++ app/src/main/java/com/monkey/focus_app/ui/home/HomeViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.monkey.focus_app.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.monkey.focus_app.data.AppRepository
+
+class HomeViewModelFactory(
+    private val repository: AppRepository
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
